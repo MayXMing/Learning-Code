caec3268af0dedf3ffc7a27a288ee0d98490dc51
MayXMing <105731118+MayXMing@users.noreply.github.com>
2022-05-28 06:26:18 +0800

    Add files via upload

---
+++ 变量的使用2022.05.27/变量的使用2022.05.27/变量的使用.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+	int a = 0;
+	
+
+	cout << "a=" << a << endl;
+	
+	a = 50;
+	
+	cout << "a=" << a << endl;
+
+	
+	
+	system("pause");
+	
+	return 0;
+
+}
---
+++ 常量的使用2022.05.27/常量的使用2022.05.27/常量的使用2022_05_27_01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#define hour 24
+
+using namespace std;
+
+
+
+int main() {
+
+	const int month = 12;
+
+	cout << "一年总共有：" << month << "月" << endl;
+
+
+	cout << "一天总共有：" << hour << "小时" << endl;
+
+
+	system("pause");
+
+	return 0;
+
+}
